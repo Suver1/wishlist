3dc997e18b24e7073c68db7827020ec1438d54b9
Andreas <andreashellesvik@hotmail.com>
2023-09-25 22:19:13 +0200

    Add heading with pulsating icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Script
+        src="https://kit.fontawesome.com/6ce357bbe2.js"
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
+      />
+      <h1 className={styles.heading}>
+        <div className={styles.treeIconContainer}>
+          <i
+            className={`${styles.treeIcon} ${styles.pulse} fa-solid fa-tree`}
+          ></i>
+        </div>
+        Ønskeliste Andreas
+      </h1>
       <div className={styles.center}>
         <Image
           className={styles.logo}
